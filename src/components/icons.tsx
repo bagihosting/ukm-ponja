3f7a093d2211efa98291108934e3c01c3dd9f26f
Rahman Tirta Anugrah <rahmantirta99@gmail.com>
2025-07-23 05:43:04 +0000

    Ubah Logo UKM PONJA dengan Icon kesehatan Buatkan secara handal dan crea

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,8 @@
 import type { SVGProps } from "react";
-import { LifeBuoy } from "lucide-react";
+import { Caduceus } from "lucide-react";
 
 export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
-    <LifeBuoy {...props} />
+    <Caduceus {...props} />
   ),
 };
